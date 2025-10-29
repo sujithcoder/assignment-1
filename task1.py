3bedf0afd70fe360649f01545c237fe205797dde
sujithcoder <120263152+sujithcoder@users.noreply.github.com>
2025-10-29 10:50:27 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+2n1=int(input("enter first number: "))
+n2=int(input("enter second number: "))
+
+print("addition:",n1+n2)
+print("subtraction:",n1-n2)
+print("multiplication:",n1*n2)
+print("division:",n1//n2)
---
+++ task2.py
@@ -0,0 +1,3 @@
+n1=input("enter your first name: ")
+n2=input("enter your last name: ")
+print(f"hello {n1} {n2} welcome to the python class")
